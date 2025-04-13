2a64093bc8453957b4f1bfeef1e2f158d869748c
Christian Sterngren <c.sterngren@gmail.com>
2025-04-13 16:21:33 +0200

    fixed lint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,39 @@
-import angular from "@angular-eslint/eslint-plugin";
-import angularTemplate from "@angular-eslint/eslint-plugin-template";
-import { parser as tsParser } from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import { resolve } from "path";
+import angular from '@angular-eslint/eslint-plugin';
+import angularTemplate from '@angular-eslint/eslint-plugin-template';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { resolve } from 'path';
 
 export default [
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: resolve("./tsconfig.json"),
-        sourceType: "module",
+        project: resolve('./tsconfig.json'),
+        sourceType: 'module',
       },
     },
     plugins: {
-      "@angular-eslint": angular,
-      "@typescript-eslint": tsPlugin,
+      '@angular-eslint': angular,
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
       ...angular.configs.recommended.rules,
-      ...angular.configs["template/process-inline-templates"].rules,
       ...tsPlugin.configs.recommended.rules,
-      "@typescript-eslint/consistent-type-definitions": "error",
-      "@typescript-eslint/no-invalid-this": ["warn"],
-      "@angular-eslint/no-host-metadata-property": "off",
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/no-invalid-this': ['warn'],
+      '@angular-eslint/no-host-metadata-property': 'off',
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     languageOptions: {
-      parser: angularTemplate.parsers[".html"],
-      sourceType: "module",
+      parser: await angularTemplate.defineParser,
+      sourceType: 'module',
     },
     plugins: {
-      "@angular-eslint/template": angularTemplate,
-      "simple-import-sort": simpleImportSort,
+      '@angular-eslint/template': angularTemplate,
     },
     rules: {
       ...angularTemplate.configs.recommended.rules,
